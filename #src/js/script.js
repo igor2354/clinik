05c70115506c6fd475c1a0c7e1d8f76b6b1bf1c5
Igor Melnikov <igor2354@yandex.ru>
2022-10-02 00:15:42 +0300

    1

--- #src/js/script.js
+++ #src/js/script.js
@@ -2,9 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
 	let sliderMainScreen = new Swiper(".slider-main", {
 		slidesPerView: 1,
 		watchOverflow: true,
+		loop: true,
+		effect: "fade",
+		fadeEffect: {
+			crossFade: true,
+		},
 		navigation: {
 			nextEl: ".main-screen .slider-arrow-next",
 			prevEl: ".main-screen .slider-arrow-prev",
+		},
+	});
+
+	let sliderInfo = new Swiper(".info-slider", {
+		slidesPerView: 1,
+		watchOverflow: true,
+		navigation: {
+			nextEl: ".slider-info .slider-arrow-next",
+			prevEl: ".slider-info .slider-arrow-prev",
 		},
 	});
 });
