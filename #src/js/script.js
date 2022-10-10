1b14ec23bead7eb94fdd4702271ac5b41c0e9eed
Igor Melnikov <igor2354@yandex.ru>
2022-10-10 23:41:17 +0300

    popup

--- #src/js/script.js
+++ #src/js/script.js
@@ -22,6 +22,16 @@ document.addEventListener("DOMContentLoaded", function () {
 		},
 	});
 
+	let sliderDoctorsPopup = new Swiper(".doctors-popup__slider", {
+		slidesPerView: "auto",
+		watchOverflow: true,
+		watchSlidesProgress: true,
+		navigation: {
+			nextEl: ".doctors-popup__slider .slider-arrow-two-next",
+			prevEl: ".doctors-popup__slider .slider-arrow-two-prev",
+		},
+	});
+
 	let arrInputPlaceholder = Array.prototype.slice.call(document.querySelectorAll(".js-placeholder"));
 
 	if (arrInputPlaceholder.length > 0) {
@@ -40,6 +50,68 @@ document.addEventListener("DOMContentLoaded", function () {
 					}
 				});
 			}
+		});
+	}
+
+	// Открытие попапов МОЖНО УДАЛЯТЬ
+	let popupAllElem = Array.prototype.slice.call(document.querySelectorAll(".modal"));
+	let openButton = Array.prototype.slice.call(document.querySelectorAll(".js-modal-show"));
+	let closeButton = Array.prototype.slice.call(document.querySelectorAll(".js-modal-close"));
+	let body = document.querySelector("body");
+
+	function openPopup(e) {
+		e.preventDefault();
+		let modal = document.querySelector(`#${e.target.dataset.popup}`);
+		modal.classList.add("active");
+
+		body.classList.add("lock");
+
+		setTimeout(() => {
+			modal.style.opacity = "1";
+		}, 100);
+	}
+
+	function closePopup() {
+		popupAllElem.forEach((element) => {
+			if (element.classList.contains("active")) {
+				let modal = element;
+
+				setTimeout(() => {
+					modal.classList.remove("active");
+				}, 300);
+
+				modal.style.opacity = "0";
+
+				body.classList.remove("lock");
+			}
+		});
+	}
+
+	if (openButton != null) {
+		openButton.forEach((element) => {
+			element.addEventListener("click", (e) => {
+				closePopup(e);
+
+				openPopup(e);
+			});
+		});
+	}
+
+	if (closeButton != null) {
+		closeButton.forEach((element) => {
+			element.addEventListener("click", (e) => {
+				closePopup();
+			});
+		});
+	}
+
+	if (popupAllElem != null) {
+		popupAllElem.forEach((element) => {
+			element.addEventListener("click", (e) => {
+				if (e.target.parentNode.classList.contains("modal")) {
+					closePopup();
+				}
+			});
 		});
 	}
 });
